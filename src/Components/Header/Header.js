1d682846b9c97d9be7a4d7050d9507877a4425aa
Eh.Emon <eh.emon3059@gmail.com>
2022-10-28 22:56:25 +0600

    home page design

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Pro Programmer</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Button,  } from 'react-bootstrap';
+import './Home.css';
+
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <div className="title mt-5 text-center">
+                <h3 className='text-success'>Pro Programmer</h3>
+                <p className='details-text'>Great programmers are rare. Their productivity is 3 times that of an average developer and 10 times that of a bad developer*. The top 1% of developers in the world don’t just write solid code but have important intangible traits. After working with thousands of developers, we have been able to identify 7 qualities that set a great programmer apart from the crowd.</p>
+            </div>
+            <div className="buttons text-center">
+                <Button variant="outline-primary" >SignUp</Button>
+                <Button variant="outline-success" className='ms-3'>Login</Button>
+            </div>
+            
         </div>
     );
 };
