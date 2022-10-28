e4a1f79391aa1c612a5902654a8dc1057ac8d0a8
Eh.Emon <eh.emon3059@gmail.com>
2022-10-28 19:38:30 +0600

    pdf download complete

--- src/Components/SingleCard/SingleCard.js
+++ src/Components/SingleCard/SingleCard.js
@@ -4,6 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import toast, { Toaster } from 'react-hot-toast';
+import jsPDF from 'jspdf';
 
 const SingleCard = () => {
   
@@ -14,6 +15,15 @@ const SingleCard = () => {
     const handelenroll =() =>{
         toast.success('Successfully enroll !');
     }
+
+    const handelePDF = () => {
+        let doc = new jsPDF("p","pt","a4");
+        doc.html(document.querySelector("#content"),{
+            callback: function(pdf){
+                pdf.save("mypdf.pdf");
+            }
+        })
+    }
     
     return (
         <div><Toaster/>
@@ -22,13 +32,15 @@ const SingleCard = () => {
             <Card.Img variant="top" src={detlis.img_URL} />
             <Card.Body>
                 <Card.Title>{detlis.course_name}</Card.Title>
-                <Card.Text>
+                <Card.Text id="content">
                 {detlis.details}
                 </Card.Text>
                 
                         <Button onClick={handelenroll} variant="primary">enroll</Button>
+                        
                     
             </Card.Body>
+            <Button onClick={handelePDF} variant="primary">Download Coure Details PDF</Button>
         </Card>
         </div>
         </div>
